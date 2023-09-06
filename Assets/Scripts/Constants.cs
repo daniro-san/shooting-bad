b531eacea65efd9236d80a0c3bcac85455a4cfbf
Vinicius Higa <viniciushigadev@gmail.com>
2023-09-05 21:31:54 -0300

    Add rigidbody movement

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Constants
+{
+    public static float WALK_SPEED = 5.0f;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class Player : MonoBehaviour {
 
     private Camera camera;
+    private Rigidbody rigidbody;
 
     void Start() {
         camera = Camera.main;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update() {
@@ -18,45 +21,23 @@ public class Player : MonoBehaviour {
 
     /// <summary>
     /// Aqui nós movemos o personagem em cada eixo.
-    /// A fórmula será sempre: Novo_eixo = tempo_do_frame * velocidade_de_mov + Eixo_antigo * (+1 ou -1),
-    /// onde -1 ou +1 servirá para atualizar o espaço ou em outras palavras "mover para frente" ou "mover para trás".
+    /// A fórmula será sempre: Vetor_velocidade = Vetor(Eixo X normalizado {-1, 1} por Input, 0, Eixo Y normalizado {-1, 1} por Input) * Velocidade_Media
     /// </summary>
     private void Move() {
-        Vector3 position = transform.position;
-
-        if (Input.GetKey(KeyCode.W)) {
-            position.z += Time.deltaTime * Constants.WALK_SPEED;
-        }
-
-        if (Input.GetKey(KeyCode.S)) {
-            position.z -= Time.deltaTime * Constants.WALK_SPEED;
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            position.x += Time.deltaTime * Constants.WALK_SPEED;
-        }
-
-        if (Input.GetKey(KeyCode.A)) {
-            position.x -= Time.deltaTime * Constants.WALK_SPEED;
-        }
-
-        Vector3 difference = position - transform.position;
-        camera.transform.position += difference;
-        transform.position = position;
+        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Constants.WALK_SPEED;
+        rigidbody.velocity = velocity;
     }
 
     /// <summary>
     /// Aqui nós fazemos o personagem olhar para o cursor do mouse.
-    /// A fórmula será a seguinte: Novo_angulo_Y = Tan(distancia ou diferença entre mouse e personagem) * 180 / Num_Pi
+    /// A fórmula será a seguinte: Novo_angulo_Y = ArcTan(distancia ou diferença entre mouse e personagem) * 180 / Num_Pi
+    /// Lembrando que o resultado da função Arco-Tangente sempre será em radianos, logo é por isso que precisamos da conversão para graus...
     /// </summary>
-    private void Look() {
+    private void Look() 
+    {
         Vector3 position = camera.WorldToScreenPoint(transform.position);
         Vector3 difference = Input.mousePosition - position;
         float angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, -angle, 0f);
-    }
-
-    private struct Constants {
-        public static float WALK_SPEED = 5.0f;
     }
 }
